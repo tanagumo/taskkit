281994fada614ceab13a886950406b53c04805ff
saryou <saryou.ssk@gmail.com>
2023-01-05 12:45:29 +0000

    make Worker implements Service protocol and now WorkerThread is a subclass of ServiceTherad.

--- taskkit/threads.py
+++ taskkit/threads.py
@@ -7,37 +7,6 @@ from .utils import logger
 from .worker import Worker
 
 
-class WorkerThread(Thread):
-    def __init__(self,
-                 group: str,
-                 backend: Backend,
-                 handler: TaskHandler,
-                 **kwargs):
-        self.group = group
-        self.worker = Worker(group, backend, handler)
-        self.should_stop = False
-        self.paused = False
-        super().__init__(**kwargs)
-
-    @property
-    def id(self) -> str:
-        return self.worker.id
-
-    def get_id(self) -> str:
-        return self.worker.id
-
-    def run(self):
-        logger.info(f'[{self.id}] worker thread started')
-        while not self.should_stop:
-            if self.paused:
-                time.sleep(1)
-                continue
-
-            if not self.worker():
-                time.sleep(1)
-        logger.info(f'[{self.id}] worker thread stopped')
-
-
 class ServiceThread(Thread):
     def __init__(self,
                  service: Service,
@@ -60,3 +29,34 @@ class ServiceThread(Thread):
                     'Unexpected exception occurred during service call '
                     f'({self.service.__class__.__qualname__})')
                 time.sleep(1)
+
+
+class WorkerThread(ServiceThread):
+    def __init__(self,
+                 group: str,
+                 backend: Backend,
+                 handler: TaskHandler,
+                 **kwargs):
+        self.group = group
+        self.worker = Worker(group, backend, handler)
+        super().__init__(service=self.worker, **kwargs)
+
+    @property
+    def id(self) -> str:
+        return self.worker.id
+
+    def get_id(self) -> str:
+        return self.worker.id
+
+    @property
+    def paused(self) -> bool:
+        return self.worker.paused
+
+    @paused.setter
+    def paused(self, paused: bool):
+        self.worker.paused = paused
+
+    def run(self):
+        logger.info(f'[{self.id}] worker thread started')
+        super().run()
+        logger.info(f'[{self.id}] worker thread stopped')
